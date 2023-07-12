f46d58aa87a4d8ffdeaf2ec1e9d791897920ead6
jungeun <sm2110147wje@sookmyung.ac.kr>
2023-07-12 18:35:50 +0900

    [feat]#1-diary

---
+++ src/main/java/com/wave/diary/domain/Posts.java
@@ -0,0 +1,19 @@
+package com.wave.diary.domain;
+
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+}
---
+++ src/main/java/com/wave/diary/service/posts.java
@@ -0,0 +1,11 @@
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
